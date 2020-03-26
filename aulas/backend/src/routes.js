41fc00c309d03eccefe6494c5bba10c464f39744
Joellytton mendonça dos santos <joellytton25@gmail.com>
2020-03-26 00:05:05 -0500

    primeiro commite backend

---
+++ aulas/backend/src/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+/**
+ * Query Params: Parâmetros nomeados enviados na rota aṕos "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+routes.post('/sessions', SessionController.store);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+module.exports = routes;
